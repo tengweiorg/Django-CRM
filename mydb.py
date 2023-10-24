358ebee68a23492852d2a95fd9d6a07fe70d7228
David Teng <davidtengw@outlook.com>
2023-10-24 20:42:31 +0800

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='tw123456'
+)
+
+cursorObject = dataBase.cursor()
+
+cursorObject.execute("CREATE DATABASE elderco")
+
+print("All Done!")
+
